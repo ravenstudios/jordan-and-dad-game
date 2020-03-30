565a24bc5469a9fec1b15279b43d996469c9ef5d
Rob <ravenstudiosrob@yahoo.com>
2020-03-30 16:08:37 -0500

    added boilerplate for pygame

---
+++ main.py
@@ -0,0 +1,40 @@
+from constants import *
+import pygame
+
+
+
+clock = pygame.time.Clock()
+surface = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
+
+pygame.init()
+
+
+def main():
+    running = True
+
+    while running:
+        clock.tick(TICK_RATE)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        draw()
+        update()
+
+    pygame.quit()
+
+
+
+def draw():
+    surface.fill((0, 0, 0))#background
+    pygame.draw.rect(surface, (RED), (50, 50, 50, 50))
+    pygame.display.flip()
+
+
+
+def update():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
