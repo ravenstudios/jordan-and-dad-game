3f92913b5599ad8f3e89be5a17798e697b885429
Rob <ravenstudiosrob@yahoo.com>
2020-03-30 16:49:57 -0500

    added hurdel

--- hurdel.py
+++ hurdel.py
@@ -4,15 +4,18 @@ import pygame
 class Hurdel:
 
     def __init__(self):
-        pass
+        self.width = BLOCK_SIZE // 2
+        self.height = BLOCK_SIZE * 1.5
+        self.x = GAME_WIDTH
+        self.y = GAME_HEIGHT - self.height
+        self.speed = -5
 
 
     def update(self):
-        pass
+        if self.x + self.width  < 0:
+            self.x = GAME_WIDTH
+        self.x += self.speed
 
 
     def draw(self, surface):
-        pass
-
-    def jump():
-        pass
+        pygame.draw.rect(surface, RED, (self.x, self.y, self.width, self.height))
